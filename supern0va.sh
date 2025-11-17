0394fac682d912ba0048285f95876e0c04687b61
WiiPrgm <support@byterenew.com>
2025-11-16 21:50:21 -0600

    Update supern0va.sh
    Added game list option

--- supern0va.sh
+++ supern0va.sh
@@ -1,10 +1,44 @@
-dd if=$1 bs=4096 count=8 status=none | strings -n 6 | sed -n "${2}p"
-SKIP=$((1150264+(1150000*($2-1))))
-dd if="$1" bs=4096 skip=$SKIP count=1147488 conv=swab status=none \
-| openssl enc -d -des-ede3-ecb -K 92072A6B1C6BE373A4023E7ABA86153E1007FEE35B689BCB -nopad \
-| dd of="$1.$2.out.img" bs=4096 conv=swab status=progress
-mv "$1.$2.out.img" "$(dd if="$1.$2.out.img" bs=1 count=64 skip=8 status=none | strings).iso"
+numlist() {
+    local hddimage="$1"
+    dd if="$hddimage" bs=4096 count=8 status=none | strings -n 6 | cat -n
+}
 
-#Usage supern0va.sh $1 $2
-#$1=name of HDD image 
-#$2=bank number you want to extract
+
+bankextract() {
+    local hddimage="$1"
+    local banknum="$2"
+
+    dd if="$hddimage" bs=4096 count=8 status=none | strings -n 6 | sed -n "${banknum}p"
+
+    SKIP=$((1150264 + (1150000 * (banknum - 1))))
+
+    dd if="$hddimage" bs=4096 skip=$SKIP count=1147488 conv=swab status=none \
+    | openssl enc -d -des-ede3-ecb \
+        -K 92072A6B1C6BE373A4023E7ABA86153E1007FEE35B689BCB \
+        -nopad \
+    | dd of="$hddimage.$banknum.out.img" bs=4096 conv=swab status=progress
+
+    mv "$hddimage.$banknum.out.img" "$(dd if="$hddimage.$banknum.out.img" bs=1 count=64 skip=8 status=none | strings).iso"
+}
+
+case "$1" in
+    list|-l)
+        numlist "$2"
+        ;;
+    extract|-x)
+        bankextract "$2" "$3"
+        ;;
+
+	help|--h|-h)
+	echo This tool can extract individual images from a Starlight Wii HDD dump.
+	echo There are 2 available options in this releaase.
+	echo $0 -l [HDD image] will list all games from the HDD header.
+	echo $0 -x [HDD image] [Bank Number] will extract that game from the HDD image.
+	exit 1
+	;;
+
+	*)
+        echo run $0 -h for usage instructions
+        exit 1
+        ;;
+esac
